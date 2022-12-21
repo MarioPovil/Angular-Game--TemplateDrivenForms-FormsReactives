4b8a7b64e14c292fb614fe5950efd7c03e9482c2
Mario Povil <marioapa2@hotmail.com>
2022-12-21 11:21:38 -0600

    v6
    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactReactiveComponent } from './contact-reactive/contact-reactive.co
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HeaderComponent } from './header/header.component';
     ContactComponent,
     ContactReactiveComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
